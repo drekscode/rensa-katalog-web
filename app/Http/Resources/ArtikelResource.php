5086f85daf22f875c83ef55c2842ab946cbb84ba
fahry <fahry.fauzan@gmail.com>
2026-03-08 20:49:42 +0700

    fix

--- app/Http/Resources/ArtikelResource.php
+++ app/Http/Resources/ArtikelResource.php
@@ -15,6 +15,8 @@ class ArtikelResource extends JsonResource
             'judul' => $this->judul,
             'slug' => $this->slug,
             'foto' => $this->foto && (str_starts_with($this->foto, 'http') || str_starts_with($this->foto, 'data:')) ? $this->foto : ($this->foto ? asset('storage/' . $this->foto) : null),
+            'foto' => $this->foto2 && (str_starts_with($this->foto2, 'http') || str_starts_with($this->foto2, 'data:')) ? $this->foto2 : ($this->foto2 ? asset('storage/' . $this->foto2) : null),
+            'foto' => $this->foto3 && (str_starts_with($this->foto3, 'http') || str_starts_with($this->foto3, 'data:')) ? $this->foto3 : ($this->foto3 ? asset('storage/' . $this->foto3) : null),
             'deskripsi' => $this->deskripsi,
             'hastag_kategori' => $this->hastag_kategori,
             'date' => $this->date,
