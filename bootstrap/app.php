b973441267fe64b417eb9145c2bc5d26700c1498
Fahry Fauzan <fahry.fauzan@gmail.com>
2026-07-10 22:31:21 +0700

    feat: add CSRF exemption for API routes and implement paginated product listing endpoint

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
+        $middleware->validateCsrfTokens(except: [
+            'api/*', // pastikan ini ada jika pakai CSRF
+        ]);
         $middleware->redirectTo(
             guests: '/admin/login',
             users: '/admin/dashboard'
